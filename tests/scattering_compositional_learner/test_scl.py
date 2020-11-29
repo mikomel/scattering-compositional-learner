d91f35e56fff62c1968a2819451ce922caa26863
mikomel <mikolaj.malkinski@gmail.com>
2020-11-29 11:58:25 +0100

    Add unit tests with torchtest

--- tests/scattering_compositional_learner/test_scl.py
+++ tests/scattering_compositional_learner/test_scl.py
@@ -1,5 +1,7 @@
 import pytest
 import torch
+import torch.nn.functional as F
+import torchtest
 
 from scattering_compositional_learner import ScatteringCompositionalLearner
 
@@ -7,6 +9,15 @@ from scattering_compositional_learner import ScatteringCompositionalLearner
 @pytest.mark.parametrize('image_size', [80, 160])
 def test_forward(image_size):
     x = torch.rand(4, 16, image_size, image_size)
+    y = torch.randint(8, (4,), dtype=torch.long)
     scl = ScatteringCompositionalLearner(image_size=image_size)
-    logits = scl(x)
-    assert logits.shape == (4, 8)
+    optimiser = torch.optim.Adam(scl.parameters())
+    torchtest.test_suite(
+        model=scl,
+        loss_fn=F.cross_entropy,
+        optim=optimiser,
+        batch=[x, y],
+        test_inf_vals=True,
+        test_nan_vals=True,
+        test_vars_change=True
+    )
